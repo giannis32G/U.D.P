6242e6fe8c1fbac1d83e2826e6a8fedfa1f76b54
greChris <nionioskoledrianos@outlook.com.gr>
2024-08-27 22:05:11 +0300

    s

--- udp/base/views.py
+++ udp/base/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm 
+
+def authView(request):
+    form = UserCreationForm()
+    return render(request, "registration/signup.html", {"form" : form})
