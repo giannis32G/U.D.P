957d4167e4924126e162eff8e568b60ba8cf89e3
greChris <nionioskoledrianos@outlook.com.gr>
2024-08-27 21:57:30 +0300

    sss

--- udp/base/urls.py
+++ udp/base/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path
-from .views import authView, home
+from django.urls import path,include
+
 urlpatterns = [
-    path("login/",authView)
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
  
