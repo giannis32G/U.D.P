941922f099b12056c48c23c4932a433ce75a784a
greChris <nionioskoledrianos@outlook.com.gr>
2024-08-29 18:20:33 +0300

    Ss

--- udp/base/views.py
+++ udp/base/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def home(request):
+    return render(request, "home.html", {"form" : form})
 
 def authView(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        else:
+            form = UserCreationForm()
     return render(request, "registration/signup.html", {"form" : form})
